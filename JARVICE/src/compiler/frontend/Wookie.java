c7566b2994ce796cc2b1665651442797be948fdc
rob0229 <rob0229@gmail.com>
2013-09-16 19:45:32 -0400

    Initial Project folder for JARVICE

---
+++ JARVICE/src/compiler/frontend/Wookie.java
@@ -0,0 +1,17 @@
+package compiler.frontend;
+
+//Main executable for JARVICE compiler
+//Programmer Rob Close
+//Created: September 16, 2013
+//Modified:
+//
+
+//accepts "Wookie" source file and compiles 
+public class Wookie {
+
+	public static void main(String[] args) {
+		System.out.println("What is a wookie doing on the planet endor!");
+
+	}
+
+}
